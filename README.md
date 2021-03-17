d020d6f745fe8b14d670ace4643183881bb84210
egrinstein <eric.grinstein@outlook.com>
2021-03-17 18:35:00 -0300

    Add speech samples

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Execute the command `$pip install -r requirements.txt`
 ## Run simulations
 
 Execute the command `$python main.py` 
+
+## Speech samples 
+The speech samples used were taken from the [VCTK dataset](https://datashare.ed.ac.uk/handle/10283/2950) created at the University of Edinburgh.
--- input_signals.py
+++ input_signals.py
@@ -1,4 +1,5 @@
 import numpy as np
+import librosa
 
 from settings import (
     SR, SIGNAL_DURATION_IN_SECONDS, LOW_FREQ_IN_HZ,
@@ -12,6 +13,10 @@ def _sinusoid(freq_in_hz):
     linear_samples = np.arange(SIGNAL_DURATION_IN_SECONDS*SR)
     return np.sin(linear_samples*freq_in_hz)
 
+def _speech():
+    return librosa.load("speech_samples/p225_001.wav", sr=SR)[0]
+
+
 def create_signal(signal_type):
     if signal_type == "low":
         return _sinusoid(LOW_FREQ_IN_HZ)
@@ -19,6 +24,8 @@ def create_signal(signal_type):
         return _sinusoid(HIGH_FREQ_IN_HZ)
     elif signal_type == "noise":
         return _noise()
+    elif signal_type == "speech":
+        return _speech()
 
 def signal_types():
-    return ["low", "high", "noise"]
+    return ["speech", "low", "high", "noise"]
