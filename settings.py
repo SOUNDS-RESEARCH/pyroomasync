2779ca615bb91194786aac2e2395495d3026f4a5
Eric Grinstein <eric.grinstein@olxbr.com>
2021-03-17 02:37:52 -0300

    Change settings

--- settings.py
+++ settings.py
@@ -10,13 +10,11 @@ FREQ_BINS = np.arange(5, 60)  # FFT bins to use for estimation
 # Room parameters
 ROOM_DIM = np.r_[10.0, 10.0]
 
+# Microphone location
+MIC_LOCATIONS = pra.circular_2D_array(ROOM_DIM / 2, 12, 0.0, 0.15)
+
 # Source parameters
 SOURCE_AZIMUTH = 61.0 / 180.0 * np.pi  # 60 degrees
 DISTANCE = 3.0  # 3 meters
 SOURCE_LOCATION = ROOM_DIM / 2 + DISTANCE * np.r_[np.cos(SOURCE_AZIMUTH), np.sin(SOURCE_AZIMUTH)]
 SOURCE_SIGNAL = np.random.randn((NFFT // 2 + 1) * NFFT)
-
-# Microphone location
-MIC_LOCATIONS = pra.circular_2D_array(ROOM_DIM / 2, 12, 0.0, 0.15)
-
-
