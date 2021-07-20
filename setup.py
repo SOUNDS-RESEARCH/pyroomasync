34a2ace6ee4925d4b8454fcc23488592096e6cb1
Eric Grinstein <eric.grinstein@outlook.com>
2021-07-20 11:56:03 -0300

    Fix setup.py to add code

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
-from setuptools import setup
+import setuptools
 
-setup(install_requires=[
-          'matplotlib',
-          'librosa',
-          'pyroomacoustics'
-      ],
-      zip_safe=False)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name='pyroomasync',
+  version='0.0.1',
+  description='Simulate asyncronous microphone array recordings',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/ImperialCollegeLondon/sap-pyroomasync',
+  author='Eric Grinstein',
+  author_email='e.grinstein@imperial.ac.uk',
+  license='MIT',
+  packages=["pyroomasync", "pyroomasync.utils"],
+  zip_safe=False,
+  install_requires=[
+    'matplotlib',
+    'librosa',
+    'pyroomacoustics'
+  ]
+)
